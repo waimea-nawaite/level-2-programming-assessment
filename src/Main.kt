32fd9630226210a1805ad91ebf0220c7f36d2808
waimea-nawaite <nawaite@waimea.school.nz>
2025-03-25 15:07:26 +1300

    done alot

--- src/Main.kt
+++ src/Main.kt
@@ -13,15 +13,16 @@
  */
 
 const val NUMBOXES = 20
-const val EMPTY = "---"
+const val EMPTY = " "
 
 
 fun main() {
     println()
     println("Old Gold game 700")
-    println("=================================================================================================================================================")
-    println("This is a two-player game, played on a one-dimensional grid with coins, * where the aim is to win by being the player who removes the gold coin.")
-    println("=================================================================================================================================================")
+    println("=========================================================================")
+    println("This is a two-player game, played on a one-dimensional grid with coins, |")
+    println("* where the aim is to win by being the player who removes the gold coin.|")
+    println("=========================================================================")
 
     println("Welcome to Old Gold!")
     println("Please enter your names for player 1 and 2: ")
@@ -29,42 +30,48 @@ fun main() {
 
     val coins = setUpBoxes()
 
-    coins.add("Coin 1")
-    coins.add("Coin 2")
-    coins.add("Coin 3")
-    coins.add("Coin 4")
-    coins.add("Coin 5")
-    coins.add("Gold")
+    coins.add("S")
+    coins.add("S")
+    coins.add("S")
+    coins.add("S")
+    coins.add("S")
+    coins.add("G")
 
     val player1 = readln()
     println("Player 1: $player1")
 
     val player2 = readln()
     println("Player 2: $player2")
+    println()
+    println("Player 1: $player1")
+    println("Player 2: $player2")
 
     println("Placing coins into the Boxes...")
 
-    coins.shuffle()
     listAllBoxes(coins)
+    coins.shuffle()
 
     layOut(coins)
     println()
+
+
+
+
 
 }
 
 fun setUpBoxes(): MutableList<String> {
     val boxList = mutableListOf<String>()
-    for (i in 1..NUMBOXES) boxList.add(EMPTY)
+    for (i in 1..NUMBOXES - 6) boxList.add(EMPTY)
     return boxList
 }
 
-fun placeCoinInBox(boxList: MutableList<String>, boxNum: Int, name: String) {
+/*fun placeCoinInBox(boxList: MutableList<String>, boxNum: Int, name: String) {
     println("Putting $name into box $boxNum")
     boxList[boxNum - 1] = name
-}
+}*/
 
 fun listAllBoxes(boxList: List<String>) {
-
     for (i in 0..<boxList.size) {
         if (boxList[i] != EMPTY) {
             println("- ${boxList[i]}")
@@ -86,12 +93,25 @@ fun listEmptyBoxes(boxList: List<String>) {
 
 fun layOut(boxList: List<String>) {
 
-    println("+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+")
+
+    println("+---".repeat(20) + "+")
     for (i in 0..boxList.size - 1) {
-        print("| ${boxList[i]}".padEnd(4))
+        print("| ${i + 1}".padEnd(4))
     }
-    println(" |")
-    println("+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+")
+    println("|")
+    println("+---".repeat(20) + "+")
+    for (i in 0..boxList.size - 1) {
+        print("| ${boxList[i]} ")
+    }
+    println("|")
+    println("+---".repeat(20) + "+")
+
+}
+fun swapBoxes(boxList: MutableList<String>, boxNum1: Int, boxNum2: Int) {
+
+    val swap = readln().toInt()
+    println("What coin do you want to move? ")
+
 }
 
 
